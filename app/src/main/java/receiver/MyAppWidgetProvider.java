aa6a8baf8ffc4a3679b8897b1589e6c8be0a752e
Anxi Wang <anxi_wang@126.com>
2017-03-01 13:39:57 +0800

    Add appwidget

--- app/src/main/java/receiver/MyAppWidgetProvider.java
+++ app/src/main/java/receiver/MyAppWidgetProvider.java
@@ -1,10 +1,27 @@
 package receiver;
 
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
 
 /**
  * Created by anxi on 2017/2/22.
  */
 
 public class MyAppWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+    }
+
 }
